566442544c80d51ccc97c5b7b04e45f123c56369
mantal <mantal0@outlook.fr>
2021-02-25 23:39:23 +0100

    Add world

--- Groww/Program.cs
+++ Groww/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Groww.Buildings;
 
 namespace Groww
@@ -9,52 +7,65 @@ namespace Groww
 	{
 		public Time Time { get; private set; }
 		public Tribe Tribe { get; private set; }
+		public World.World World { get; private set; }
+
+		public Game()
+		{
+			Time = new Time();
+            Tribe = new Tribe
+            {
+                Buildings = new List<Building>(),
+                Resources = new Resource[]
+                {
+                    new Resource
+                    {
+                        Name = "Food",
+                        Amount = 10,
+                        Max = 20,
+                    }
+                },
+                Humans = new List<Human>
+                {
+                    new()
+                    {
+                        Age = new Time(),
+                        Health = 100,
+                        Name = "Lynn",
+                    },
+                    new()
+                    {
+                        Age = new Time(),
+                        Health = 100,
+                        Name = "Alex",
+                    },
+                }
+            };
+
+            World = new World.World();
+		}
 
 		public void Init()
 		{
-			Time = new Time();
-			Tribe = new Tribe
-			{
-				Buildings = new List<Building>(),
-				Resources = new Resource[]
-				{
-					new Resource
-					{
-						Name = "Food",
-						Amount = 10,
-						Max = 20,
-					}
-				},
-				Humans = new List<Human>
-				{
-					new()
-					{
-						Age = new Time(),
-						Health = 100,
-						Name = "Lynn",
-					},
-					new()
-					{
-						Age = new Time(),
-						Health = 100,
-						Name = "Alex",
-					},
-				}
-			};
+			World.Generate();
+			Tribe.PosX = 25;
+			Tribe.PosY = 25;
 		}
 
 		public void Tick()
 		{
-
+			Tribe.Tick(Time);
 		}
 	}
 
 	public class Tribe : Entity
 	{
-		public IEnumerable<Building> Buildings { get; set; }
-		public IEnumerable<Resource> Resources { get; set; }
-		public IEnumerable<Human> Humans { get; set; }
-		
+		public IEnumerable<Building> Buildings { get; set; } = new List<Building>();
+		public IEnumerable<Resource> Resources { get; set; } = new List<Resource>();
+		public IEnumerable<Human> Humans { get; set; } = new List<Human>();
+
+		public int PosX { get; set; }
+		public int PosY { get; set; }
+
 		public override void Tick(Time time)
 		{
 			foreach (var building in Buildings)
---
+++ Groww/World/Area.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+namespace Groww.World
+{
+	public class World
+	{
+		public Area[,] Areas { get; set; }
+
+		public void Generate()
+		{
+			Areas = new Area[100, 100];
+			for (var i = 0; i < Areas.GetLength(0); i++)
+			{
+				for (var j = 0; j < Areas.GetLength(1); j++)
+				{
+					Areas[i, j] = Rand.Next(2) switch
+							   {
+								   0 => new Area
+								   {
+									   Biome = new Biome
+									   {
+										   Name = "Plain",
+										   SpeedMultiplier = 1,
+									   },
+									   Resources = new []
+									   {
+										   new Food
+										   {
+											   Amount = 500,
+											   Max = 1000,
+											   Name = "Food",
+										   },
+									   },
+								   },
+								   1 => new Area
+								   {
+									   Biome = new Biome
+									   {
+										   Name = "Forest",
+										   SpeedMultiplier = .5,
+									   },
+									   Resources = new []
+									   {
+										   new Food
+										   {
+											   Amount = 1000,
+											   Max = 2000,
+											   Name = "Food",
+										   },
+									   },
+								   },
+								   _ => throw new System.NotImplementedException(),
+							   };
+				}
+			}
+		}
+	}
+
+    public class Area
+    {
+        public IEnumerable<Resource> Resources { get; set; }
+        public Biome Biome { get; set; }
+     }
+
+	public class Biome
+	{
+		public string Name { get; set; }
+		public double SpeedMultiplier { get; set; }
+	}
+}
